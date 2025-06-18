c30f2c4cf170c22ef29191b4104b2e3e865e865f
Jyoshna2001-kanakam <kanakamjyoshna1812@gmail.com>
2025-06-18 11:37:00 +0530

    Created bill app frontend

---
+++ src/components/user/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 font-semibold text-shadow-yellow-200'>Register</h1>
+      <h1 className='text-amber-900'>Home page</h1>
+    </div>
+  )
+}
+
+export default Register
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <App></App>
+)
+
+// npm install react-router-dom
+// npm install tailwindcss @tailwindcss/vite
+//Tailwind css Intellisence ==extension
